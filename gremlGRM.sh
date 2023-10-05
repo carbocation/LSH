12060196ca3daa9e197be0dd5156713fa00c201f
James Pirruccello <james@carbocation.com>
2023-10-04 20:34:16 -0700

    First pass attempt at converting LSH into a package that can be imported with Devtools in R.

--- gremlGRM.sh
+++
@@ -1,3 +0,0 @@
-
-
-/nfs/scistore13/robingrp/sojavee/gcta64 --bfile /mnt/beegfs/robingrp/sojavee/LSHResults/simM10k_N20k_NT90k --make-grm --out /mnt/beegfs/robingrp/sojavee/LSHResults/simM10k_N20k_NT90k --thread-num 5
